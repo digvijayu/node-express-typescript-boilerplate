034a0825cb50033fd1e907a6665a3e0e938c62c6
Digvijay Upadhyay <digvijay.upadhyay@roxorgaming.com>
2022-08-24 17:43:49 +0100

    feat: compiled the typescript to js files

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  let healthCheck: {
+    uptime?: number,
+    message?: string,
+    timestamp?: number
+  } = {};
+  try {
+    healthCheck = {
+      uptime: process.uptime(),
+      message: 'OK',
+      timestamp: Date.now()
+    };
+    res.json(healthCheck);
+  } catch (error: any) {
+    healthCheck.message = error.message;
+    res.status(503).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App started on port ${PORT}`);
+});
